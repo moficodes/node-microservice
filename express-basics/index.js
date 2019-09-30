be8f3bc9c7629bb423c552649550cad4948aed08
Mofizur Rahman <moficodes@gmail.com>
2019-09-30 00:13:38 -0400

    updated express basics

--- express-basics/index.js
+++ express-basics/index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+  res.type('text/plain');
+  res.send('Hello World');
+});
 
 app.listen(port, () => console.log(`example listening on port ${port}`));
